98fd161fc741aeabf13326bc32f2d5bbafb084de
danishshaikh9702 <danish.shaikh@respo.co.in>
2023-10-09 11:07:38 +0530

    adding validation package 1.0.0

---
+++ src/validation/example.ts
@@ -0,0 +1,63 @@
+// Import the Validation class
+import validation  from './index'; // Update the import path to match your project structure
+
+// Create an instance of the Validation class
+
+// Example usage
+const emailValid = 'example@email.com';
+const emailInvalid = 'invalid-email';
+
+console.log(`Email Validation: ${validation.email(emailValid)}`); // true
+console.log(`Email Validation: ${validation.email(emailInvalid)}`); // false
+
+const mobileValid1 = '+91 9702686401';
+const mobileValid2 = 9702686401;
+const mobileInvalid = '1234567890';
+
+console.log(`Mobile Number Validation: ${validation.mobileNumber(mobileValid1)}`); // true
+console.log(`Mobile Number Validation: ${validation.mobileNumber(mobileValid2)}`); // true
+console.log(`Mobile Number Validation: ${validation.mobileNumber(mobileInvalid)}`); // false
+
+const panValid = 'ABCDE1234F';
+const panInvalid = 'ABCD1234F'; // Missing a character
+
+console.log(`PAN Card Validation: ${validation.panCardNumber(panValid)}`); // true
+console.log(`PAN Card Validation: ${validation.panCardNumber(panInvalid)}`); // false
+
+const urlValid1 = 'https://www.example.com';
+const urlValid2 = 'ftp://ftp.example.com';
+const urlInvalid = 'example.com'; // Missing protocol
+
+console.log(`URL Validation: ${validation.httpUrl(urlValid1)}`); // true
+console.log(`URL Validation: ${validation.httpUrl(urlValid2)}`); // true
+console.log(`URL Validation: ${validation.httpUrl(urlInvalid)}`); // false
+
+const ifscValid = 'ABCD0123456';
+const ifscInvalid = 'ABCD12345'; // Invalid length
+
+console.log(`IFSC Code Validation: ${validation.ifscCode(ifscValid)}`); // true
+console.log(`IFSC Code Validation: ${validation.ifscCode(ifscInvalid)}`); // false
+
+const validDL1: string = 'MH0420170056170'; // Format: "MH0420170056170"
+const validDL2: string = 'MH-0420170056170'; // Format: "MH-0420170056170"
+const validDL3: string = 'MH-04 20170056170'; // Format: "MH-04 20170056170"
+const validDL4: string = 'MH04 20170056170';  // Format: "MH04 20170056170"
+const invalidDL: string = 'InvalidDL123'; // Invalid format
+
+console.log(`Driving License Validation: ${validation.drivingLisence(validDL1)}`); // true
+console.log(`Driving License Validation: ${validation.drivingLisence(validDL2)}`); // true
+console.log(`Driving License Validation: ${validation.drivingLisence(validDL3)}`); // true
+console.log(`Driving License Validation: ${validation.drivingLisence(validDL4)}`); // true
+console.log(`Driving License Validation: ${validation.drivingLisence(invalidDL)}`); // false
+
+const aadharValid1 = '1232 1223 1123';
+const aadharValid2 = '11 12 22 22 22 22';
+const aadharValid3 = '123456789012';
+const aadharValid4 = '1 2 3 4 5 6 7 8 9 0 1 2';
+const aadharValid5 = '11222 222 222 2';
+
+console.log(`Aadhar Card Validation: ${validation.aadharCardNumber(aadharValid1)}`); // true
+console.log(`Aadhar Card Validation: ${validation.aadharCardNumber(aadharValid2)}`); // true
+console.log(`Aadhar Card Validation: ${validation.aadharCardNumber(aadharValid3)}`); // true
+console.log(`Aadhar Card Validation: ${validation.aadharCardNumber(aadharValid4)}`); // true
+console.log(`Aadhar Card Validation: ${validation.aadharCardNumber(aadharValid5)}`); // true
---
+++ src/validation/index.ts
@@ -0,0 +1,42 @@
+class Validation {
+    
+    email(email:string): boolean{
+        const emailRegex: RegExp = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        return emailRegex.test(email);
+    }
+
+    mobileNumber(mobileNumber: string | number): boolean{
+        const mobileStr = mobileNumber.toString().replace(/\s/g, '');
+        const mobileRegex = /^(\+91)?\d{10}$/;
+        return mobileRegex.test(mobileStr);
+    } 
+
+    panCardNumber(panNumber:string):boolean{
+        const panRegex:RegExp = /^[A-Z]{5}[0-9]{4}[A-Z]$/;
+        return panRegex.test(panNumber);
+    }
+
+    httpUrl(url:string):boolean{
+        const urlRegex:RegExp = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
+        return urlRegex.test(url);      
+    }
+
+    ifscCode(ifscCode:string):boolean{
+        const ifscRegex:RegExp = /^[A-Z]{4}[0][A-Z0-9]{6}$/;
+        return ifscRegex.test(ifscCode);
+    }
+
+    drivingLisence(DLNumber: string): boolean{
+        let dlRegex = new RegExp(/^(([A-Z]{2}[0-9]{2})( )|([A-Z]{2}-[0-9]{2}))((19|20)[0-9][0-9])[0-9]{7}$/)
+        return dlRegex.test(DLNumber);
+    }
+
+    aadharCardNumber(aadharNumber: string): boolean {
+        const trimAadharNumber=aadharNumber.split(' ').join('')
+        const aadharRegex = /^(\d{12})$/;
+        return aadharRegex.test(trimAadharNumber);
+    }  
+    
+}
+const validation=new Validation()
+export default validation 
